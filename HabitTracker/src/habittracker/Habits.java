8246530cee8828abc5effe9a3e0c024026e440ab
ConnerEdgell2 <conneredgell5@gmail.com>
2019-08-26 12:11:10 -0400

    new file

---
+++ HabitTracker/src/habittracker/Habits.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package habittracker;
+
+/**
+ *
+ * @author conne
+ */
+public class Habits {
+    //this is where we can have user input for personalized habits
+    
+}
