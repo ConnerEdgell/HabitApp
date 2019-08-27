db10d55008dec8cdc899bab0261833e443aeebbf
astrojw <jacob.winemiller@gmail.com>
2019-08-27 13:06:31 -0400

    interface class barebones

--- HabitTracker/src/habittracker/HabitInterface.java
+++ HabitTracker/src/habittracker/HabitInterface.java
@@ -12,7 +12,27 @@ package habittracker;
 
 import javax.swing.*;
 import java.awt.*;
+
 public class HabitInterface {
     //this class should contain the code for the gui
+ public static void main(String[] args){   
+    //create  windows dimensions
+    final int window_height = 500;
+    final int window_width = 500;
     
+    //creates JFRAME
+    JFrame window = new JFrame();
+    
+    //set title
+    window.setTitle("HabitApp");
+    
+    //set window size
+    window.setSize(window_height, window_width);
+    
+    //close window
+    window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+    
+    window.setVisible(true);
+    
+ }
 }
