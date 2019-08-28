20c24be73fc2a07b8dbf506b04c4dcbe7aeb38cc
astrojw <jacob.winemiller@gmail.com>
2019-08-28 11:29:30 -0400

    gui

--- HabitTracker/src/habittracker/HabitTracker.java
+++ HabitTracker/src/habittracker/HabitTracker.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package habittracker;
+import java.awt.BorderLayout;
 import javax.swing.*;
 /**
  *
@@ -20,6 +21,40 @@ public class HabitTracker {
         System.out.println("Hello World");
         System.out.println("habit");
         System.out.println("woo baby");
+        System.out.println("yeee");
+        
+         final int height = 1000;
+     final int width = 1000;
+     
+     JFrame frame = new JFrame("HabitApp");
+     
+     frame.setSize(height,width);
+     
+     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+     frame.setVisible(true);
+     
+     
+     //Adding Menu Bar
+     JMenuBar menu = new JMenuBar();
+     
+     JMenu m1 = new JMenu("FILE");
+     
+     menu.add(m1);
+     
+     JMenuItem habit = new JMenuItem("Habits");
+     JMenuItem schedule = new JMenuItem("Schedule");
+     JMenuItem analytics = new JMenuItem("Analytics");
+     
+     m1.add(habit);
+     m1.add(schedule);
+     m1.add(analytics);
+     
+     frame.getContentPane().add(BorderLayout.NORTH, menu);
+     frame.setVisible(true);
+        
+        
+        
+        
     }
 
 }
