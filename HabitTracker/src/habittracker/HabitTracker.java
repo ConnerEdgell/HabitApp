a3a427a323e8a1254dde1058332957db59214c8c
astrojw <jacob.winemiller@gmail.com>
2019-08-28 11:57:40 -0400

    gui in habit interface

--- HabitTracker/src/habittracker/HabitTracker.java
+++ HabitTracker/src/habittracker/HabitTracker.java
@@ -18,40 +18,7 @@ public class HabitTracker {
     public static void main(String[] args) {
         // TODO code application logic 
 
-        
-        System.out.println("Hello World");
-        System.out.println("habit");
-        System.out.println("woo baby");
-        System.out.println("yeee");
-        
-         final int height = 1000;
-     final int width = 1000;
-     
-     JFrame frame = new JFrame("HabitApp");
-     
-     frame.setSize(height,width);
-     
-     frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-     frame.setVisible(true);
-     
-     
-     //Adding Menu Bar
-     JMenuBar menu = new JMenuBar();
-     
-     JMenu m1 = new JMenu("FILE");
-     
-     menu.add(m1);
-     
-     JMenuItem habit = new JMenuItem("Habits");
-     JMenuItem schedule = new JMenuItem("Schedule");
-     JMenuItem analytics = new JMenuItem("Analytics");
-     
-     m1.add(habit);
-     m1.add(schedule);
-     m1.add(analytics);
-     
-     frame.getContentPane().add(BorderLayout.NORTH, menu);
-     frame.setVisible(true);
+        new HabitInterface();
         
         
              
