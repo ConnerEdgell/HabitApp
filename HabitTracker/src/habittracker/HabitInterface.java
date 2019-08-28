04eb523422e084f3fe8b3399844047c6151d37db
astrojw <jacob.winemiller@gmail.com>
2019-08-28 12:22:41 -0400

    gui in habit interface

--- HabitTracker/src/habittracker/HabitInterface.java
+++ HabitTracker/src/habittracker/HabitInterface.java
@@ -15,24 +15,45 @@ import java.awt.*;
 
 public class HabitInterface extends HabitTracker {
     //this class should contain the code for the gui
+    
+    final int height = 1500;
+    final int width = 1500;
+    JFrame frame = new JFrame();
+    
+    
+    public HabitInterface(){
+        
+        frame.setTitle("HabitApp");
+        frame.setSize(height,width);
+        frame.setLayout(new BorderLayout());
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+    
+    public void buildJMenu(){
+     JMenuBar menu = new JMenuBar();
+     
+     JMenu m1 = new JMenu("FILE");
+     
+     menu.add(m1);
+     
+     JMenuItem habit = new JMenuItem("Habits");
+     JMenuItem schedule = new JMenuItem("Schedule");
+     JMenuItem analytics = new JMenuItem("Analytics");
+     
+     m1.add(habit);
+     m1.add(schedule);
+     m1.add(analytics);
+     
+     frame.getContentPane().add(BorderLayout.NORTH, menu);
+     frame.setVisible(true);
+        
+    }
+    
  public static void main(String[] args){   
     //create  windows dimensions
-    final int window_height = 500;
-    final int window_width = 500;
     
-    //creates JFRAME
-    JFrame window = new JFrame();
-    
-    //set title
-    window.setTitle("HabitApp");
-    
-    //set window size
-    window.setSize(window_height, window_width);
-    
-    //close window
-    window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
-    
-    window.setVisible(true);
-    
+  
+  
  }
 }
